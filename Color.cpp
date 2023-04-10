687317ab548f37b0393d418f7109399c2c5f53f8
José Ovando <42974480+Jos260400@users.noreply.github.com>
2023-04-10 07:16:28 +0200

    Add files via upload

---
+++ Color.cpp
@@ -0,0 +1,32 @@
+//Importamos cada una de las librerias
+
+//Con respecto a nuestro encabezado
+
+#include<stdio.h>
+#include<conio.h>
+
+//Para cada una de nuestras figuras
+#include<graphics.h>
+#include<dos.h>
+
+//Inicializamos
+void main()
+{
+	int g = DETECT,o;
+	int x,y,a;
+	initgraph(&g, &o, "C:\\Turboc3\\BGI");
+	x=getmaxx()/2;
+	y=getmaxy()/2;
+	for(a=30;a<200;a++)
+	
+	{
+		delay(100);
+		setcolor(a/10);
+		arc(x,y,0,180,a-10);
+	}
+	
+	getch();
+	
+}
+
+
